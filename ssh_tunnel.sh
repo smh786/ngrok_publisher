9bd578772798938c9e2039d870f7925d9f4a8441
Syed Hasan <syed.hasan@digicatapult.org.uk>
2022-09-12 15:55:03 +0100

    bash to das

--- ssh_tunnel.sh
+++ ssh_tunnel.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 host=`conda run --no-capture -n ngrok_publisher python main.py host`
+port=`conda run --no-capture -n ngrok_publisher python main.py port`
 
-echo $host
+ssh root@$host -p $port
