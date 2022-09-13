41c8af20de84299eb4c7ca9116692a46520c31cf
Syed Hasan <syed.hasan@digicatapult.org.uk>
2022-09-13 18:07:06 +0100

    its a madness

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 from Publisher.Publisher import Publisher
 import sys
-
+import os
 
 def main():
     # print command line arguments
     p = Publisher("2EfZ5ribW1zR3kE37vMZ1bM8nXo_4KsCxoXoknrjszs2wAHUX")
+    print(str(p.host) + " " + str(p.port))
     args = sys.argv
-    if args[1] == 'host':
-        return p.host
-
-    if args[1] == 'port':
-        return p.port
-    print("Please provide a suitable arguemnt (host | port)")
+    for arg in args:
+        if arg == '-C':
+            print(f"ssh root@{p.host} -p {p.port}")
 
 
 if __name__ == "__main__":
-    print(main())
+    main()
--- ssh_tunnel.sh
+++ ssh_tunnel.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-host=`conda run --no-capture -n ngrok_publisher python main.py host`
-port=`conda run --no-capture -n ngrok_publisher python main.py port`
-
+read -r host port <<< "$(conda run --no-capture -n ngrok_publisher python main.py)"
 ssh root@$host -p $port
